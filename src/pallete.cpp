693ea4e1d12739d90ba5e596b3755d0a37adb2eb
César Rodríguez <cesar.idaho@gmail.com>
2020-08-05 23:49:25 +0200

    Color linear interpolation working.

--- src/pallete.cpp
+++ src/pallete.cpp
@@ -13,10 +13,13 @@ Pallete::Pallete(){
     
     ColorPoint cp;
     //    i   r     g     b     a
-    cp = { .index = 127, .color = { .r = 0x00, .g = 0x00, .b = 0x00, .a = 0x44}};
+    cp = { .index = 10, .color = { .r = 0x10, .g = 0x10, .b = 0x10, .a = 0xff}};
     this->colorpoints.push_back(cp);
-    cp = { .index = 128, .color = { .r = 0xff, .g = 0xff, .b = 0xff, .a = 0xcc}};
+    cp = { .index = 128, .color = { .r = 0xff, .g = 0xff, .b = 0xff, .a = 0xff}};
     this->colorpoints.push_back(cp);
+    cp = { .index = 250, .color = { .r = 0x0f, .g = 0x00, .b = 0xff, .a = 0xff}};
+    this->colorpoints.push_back(cp);
+
 
     this->recalculate();
 
@@ -27,8 +30,13 @@ void Pallete::recalculate(){
     std::sort(this->colorpoints.begin(), this->colorpoints.end(), compareColorPoint);
 
     int cursor = -1;
+    int step = -1;
+    int delta = -1;
     ColorPoint first = this->colorpoints.front();
     ColorPoint last  = this->colorpoints.back();
+    //Uint8 current = first.index;
+    SDL_Color color;
+    color = { .r = 0x00, .g = 0x00, .b = 0x00, .a = 0xff};
     // we assume that the list of points always have minimum of one point
     // all colors the same from begining to first point
     do {
@@ -36,9 +44,27 @@ void Pallete::recalculate(){
         this->pallete[cursor] = first.color;
     } while (cursor < first.index );
 
-    // TODO: linear interpolation from point to point
+    // linear interpolation from point to point
+    std::vector<ColorPoint>::iterator it = this->colorpoints.begin();
+    while(it != this->colorpoints.end() - 1){
+        //printf("%u - ", it->index);
+        step = 0;
+        delta = (it + 1)->index - it->index;
+        for (cursor = it->index; cursor < (it + 1)->index; cursor++){
+            color.r = static_cast<Uint8>((float)it->color.r + (((float)(it+1)->color.r - (float)it->color.r)*((float)step/(float)delta)));
+            color.g = static_cast<Uint8>((float)it->color.g + (((float)(it+1)->color.g - (float)it->color.g)*((float)step/(float)delta)));
+            color.b = static_cast<Uint8>((float)it->color.b + (((float)(it+1)->color.b - (float)it->color.b)*((float)step/(float)delta)));
+            color.a = static_cast<Uint8>((float)it->color.a + (((float)(it+1)->color.a - (float)it->color.a)*((float)step/(float)delta)));
+            this->pallete[cursor] = color;
+            //printf("i: %u r: %u g: %u b: %u a: %u \n", cursor, color.r, color.g, color.b, color.a);
+            step++;
+        }
+
+        it++; // next color point
+    }
     
     // all colors the same from last point to end
+    cursor = static_cast<int>(last.index) - 1;
     while (cursor < 255) {
         cursor++;
         this->pallete[cursor] = last.color;
